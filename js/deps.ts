800ce4480e46b4ffb97e47503a77f4fe7906c291
Kyle June <github@kylejune.com>
2021-12-18 15:10:24 -0600

    Add skypack types

--- js/deps.ts
+++ js/deps.ts
@@ -1,20 +1,21 @@
-import React, { Fragment } from "https://cdn.skypack.dev/react@17.0.2";
-import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.2";
+import React, { Fragment } from "https://cdn.skypack.dev/react@17.0.2?dts";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.2?dts";
 
+// @deno-types="https://crux.land/7AEp6g"
 export {
   BrowserRouter,
   Link,
   NavLink,
   Route,
   Routes,
-} from "https://cdn.skypack.dev/react-router-dom@6.1.1";
+} from "https://cdn.skypack.dev/react-router-dom@6.2.1";
 
-export { tw } from "https://cdn.skypack.dev/twind@0.16.16";
+export { tw, setup } from "https://cdn.skypack.dev/twind@0.16.16";
 export {
   Disclosure,
   Menu,
   Transition,
-} from "https://cdn.skypack.dev/@headlessui/react@1.4.2";
+} from "https://cdn.skypack.dev/@headlessui/react@1.4.2?dts";
 export {
   BellIcon,
   MenuIcon,
